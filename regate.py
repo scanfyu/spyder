b5a6d43c4eb85975cbe758700f189cce8aa23588
scanfyu <songseu@outlook.com>
2017-03-07 00:56:37 +0800

    reuse

--- regate.py
+++ regate.py
@@ -6,11 +6,24 @@ import requests
 import re
 import xlwt
 import time
-import csv
 from bs4 import BeautifulSoup as bs
 from pymongo import MongoClient
 
 class gate():    
+
+    def __init__(self):
+        client = MongoClient()  # 与MongoDB建立链接
+        db = client['xuezheziliao']  # 选择一个数据库
+        self.regate_collection = db['researchgate']  # 在xuezheziliao数据库中选择集合
+        self.name = ''  # 保存学者姓名
+        self.regateurl = ''  # 保存学者主页
+        self.rg = ''  # 保存学者rg值
+        self.numbers = ''  # 保存学者相关数据
+        self.skills = ''  # 保存学者技能
+        self.topics = ''  # 保存学者主题
+        self.co_authors = ''  # 保存合作作者
+        self.following = ''  # 保存关注者
+        self.followers = ''  # 保存被关注者
 
     #解析获得用户主页姓名
     def test(self, url):
--- zhihu.py
+++ zhihu.py
@@ -16,7 +16,43 @@ def getHtml(url):
     request = urllib.request.Request(url, headers=header)
     response = urllib.request.urlopen(request)
     text = response.read()
-    print(text)
+    #print(text)
     return text
 
+# 解析日报链接
+def getUrls(html):
+    pattern = r'<a href="/story/(.*?)"',re.S  # re.S匹配换行符
+    items = re.findall(pattern, html)
+    #print(items)
+    #return items
+    urls = []  # 拼接好的链接
+    for item in items:
+        urls.append('https://daily.zhihu.com/story/'+item)
+        print(urls[-1])  # 列表最末尾元素 去掉重复链接（链接新加入）
+    return urls
+
+# 解析日报标题+正文
+def getContent(url):
+    html = getHtml(url)  # 调用函数
+    pattern = r''
+    items = re.findall(pattern, html)
+    print(items)
+
+    for items in items_withtag:  # 将内容输出为中文
+        print items
+
+def characterProcessing(html):
+    htmlParser = htmlParser
+
+
+# 去掉杂志   sub('<.*?>', '',content_tag)  从后边标签 找到第一个 用第二个参数代替
+
 html = getHtml(url)  # 调用
+
+def main():
+    url =
+    html = getHtml(url)
+    urls = 
+
+if __name__ == "__main__":  # 判断文件入口
+    main()
